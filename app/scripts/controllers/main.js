201fad13e8e2dfacd84e2370e61385d12ebf4716
Josue Zarzosa <josketres@gmail.com>
2014-09-03 01:17:11 +0200

    Agregue marcadores en el mapa para las primeras tres estaciones

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,21 +8,45 @@
  * Controller of the corruptourMtyMapaApp
  */
 angular.module('corruptourMtyMapaApp')
-  .controller('MainCtrl', function ($scope) {
+	.controller('MainCtrl', function($scope) {
 
-  	// see http://dbsgeo.com/latlon/
-	$scope.map = {
-	    center: {
+		// see http://dbsgeo.com/latlon/
+		$scope.map = {
+			center: {
+				latitude: 25.67161,
+				longitude: -100.31976
+			},
+			zoom: 13
+		};
 
-	        latitude: 25.66906,
-	        longitude: -100.35105
-	    },
-	    zoom: 14
-	};
+		$scope.createStation = function(id, name, latitude, longitude) {
+			return {
+				id: id,
+				name: id + '. ' + name,
+				options: {
+					draggable: false,
+					title: this.name,
+					labelAnchor: '10 39',
+					labelContent: name,
+					labelClass: 'labelMarker'
+				},
+				latitude: latitude,
+				longitude: longitude
+			};
+		};
 
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+		$scope.stations = [
+			$scope.createStation(0, 'Inicio', 25.67275, -100.30787),
+			$scope.createStation(1, 'Palacio de Gobierno', 25.67257, -100.30922),
+			$scope.createStation(2, 'Congreso del Estado', 25.66916, -100.31087)
+		];
+
+		$scope.awesomeThings = [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+		];
+
+
+
+	});
