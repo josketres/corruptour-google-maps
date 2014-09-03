07af5da73021c84ff7358719876aaa1ec1d9b536
Josue Zarzosa <josue.zarzosa@esailors.de>
2014-09-03 09:23:45 +0000

    Lista de estaciones completada.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,7 +18,7 @@ angular.module('corruptourMtyMapaApp')
 			},
 			zoom: 13,
 			options: {
-				scrollwheel: false
+				scrollwheel: true
 			},
 			draggable: true
 		};
@@ -29,9 +29,9 @@ angular.module('corruptourMtyMapaApp')
 				name: id + '. ' + name,
 				options: {
 					draggable: false,
-					title: this.name,
+					title: name,
 					labelAnchor: '10 39',
-					labelContent: name,
+					labelContent: id + '',
 					labelClass: 'labelMarker'
 				},
 				latitude: latitude,
@@ -42,7 +42,16 @@ angular.module('corruptourMtyMapaApp')
 		$scope.stations = [
 			$scope.createStation(0, 'Inicio', 25.67275, -100.30787),
 			$scope.createStation(1, 'Palacio de Gobierno', 25.67257, -100.30922),
-			$scope.createStation(2, 'Congreso del Estado', 25.66916, -100.31087)
+			$scope.createStation(2, 'Congreso del Estado', 25.66916, -100.31087),
+			$scope.createStation(3, 'Palacio Municipal Monterrey', 25.66477, -100.31077),
+			$scope.createStation(4, 'Barrio Antiguo', 25.66493, -100.30792),
+			$scope.createStation(5, 'Infonabit', 25.66860, -100.31065),
+			$scope.createStation(6, 'Torre VIP', 25.67573, -100.29225),
+			$scope.createStation(7, 'Parque Fundidora', 25.68232, -100.28655),
+			$scope.createStation(8, 'Par Vial', 25.66782, -100.33429),
+			$scope.createStation(9, 'Casino Royale', 25.674156, -100.354931),
+			$scope.createStation(10, 'Rio Santa Catarina', 25.66817, -100.34156),
+			$scope.createStation(11, 'Rio Santa Catarina', 25.67193, -100.31938),
 		];
 
 		$scope.awesomeThings = [
