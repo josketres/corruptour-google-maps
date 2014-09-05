451b13271de3e7edd6b086f1a9da11397ee2b04c
josketres <josketres@gmail.com>
2014-09-05 07:30:03 +0000

    Add windows to show the name of the station

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -24,7 +24,8 @@ angular.module('corruptourMtyMapaApp')
 		};
 
 		$scope.createStation = function(id, name, latitude, longitude) {
-			return {
+
+			var station = {
 				id: id,
 				name: id + '. ' + name,
 				options: {
@@ -32,16 +33,29 @@ angular.module('corruptourMtyMapaApp')
 					title: name,
 					labelAnchor: '10 39',
 					labelContent: id + '',
-					labelClass: 'labelMarker'
+					labelClass: 'labelMarker',
+					anchorPoint: google.maps.Point(40, 40)
 				},
 				latitude: latitude,
-				longitude: longitude
+				longitude: longitude,
+				show: false,
+				onClick: function() {
+					$scope.hideAllStationsWindows();
+					station.show = true;
+				}
 			};
+			return station;
+		};
+
+		$scope.hideAllStationsWindows = function() {
+			angular.forEach($scope.stations, function(value, key) {
+				value.show = false;
+			});
 		};
 
 		$scope.stations = [
 			$scope.createStation(0, 'Inicio', 25.67275, -100.30787),
-			$scope.createStation(1, 'Palacio de Gobierno', 25.67257, -100.30922),
+			$scope.createStation(1, 'Palacio de Gobierno', 25.67257, -100.30922), // 1 first to 
 			$scope.createStation(2, 'Congreso del Estado', 25.66916, -100.31087),
 			$scope.createStation(3, 'Palacio Municipal Monterrey', 25.66477, -100.31077),
 			$scope.createStation(4, 'Barrio Antiguo', 25.66493, -100.30792),
@@ -50,9 +64,11 @@ angular.module('corruptourMtyMapaApp')
 			$scope.createStation(7, 'Parque Fundidora', 25.68232, -100.28655),
 			$scope.createStation(8, 'Par Vial', 25.66782, -100.33429),
 			$scope.createStation(9, 'Casino Royale', 25.674156, -100.354931),
-			$scope.createStation(10, 'Rio Santa Catarina', 25.66817, -100.34156),
+			$scope.createStation(10, 'Isssteleon', 25.66817, -100.34156),
 			$scope.createStation(11, 'Rio Santa Catarina', 25.67193, -100.31938),
 		];
+
+		$scope.stations[0].show = true;
 
 		$scope.createPath = function(latitude, longitude) {
 			return {
